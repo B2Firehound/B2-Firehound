a44523988bbb4eb2c4b8627913827adf3025d499
B2 Firehound <clement.guidat@passy-buzenval.com>
2026-04-10 10:41:31 +0200

    V1

--- train_drone.py
+++ train_drone.py
@@ -1,18 +1,20 @@
-from ultralytics import YOLO
-import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+# B2 Firehound / Marc Girard, Clément Guidat, Paul Scheerlinck, Quentin Blachier / 2026
 
-if __name__ == '__main__':
-    model = YOLO("yolov8s.pt")
+from ultralytics import YOLO                  # Import de la librairie YOLO
+import os                                     # Import d'une librairie pour gérer des paramètres système
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'   # Évite un bug de conflit de librairies
 
-    model.train(
-        data="C:/Users/Utilisateur/Downloads/RAPACE_dataset/drone.yaml",
-        epochs=150,
-        imgsz=640,
-        batch=16,
-        lr0=0.001,
-        optimizer="AdamW",
-        mosaic=0.8,
-        scale=0.5,
-        fliplr=0.5,
+if __name__ == '__main__':                    # Lance le code seulement si il est exécuté directement
+    model = YOLO("best.pt")                   # On charge le modèle YOLO
+
+    model.train(                              # Lance l’entraînement du modèle
+        data="C:/Users/Utilisateur/Downloads/RAPACE_dataset/drone.yaml",  # Chemin vers le dataset
+        epochs=150,                           # Nombre de cycles d’entrainement
+        imgsz=640,                            # Taille des images utilisées
+        batch=16,                             # Nombre d’images traitées en même temps
+        lr0=0.001,                            # Vitesse d’apprentissage
+        optimizer="AdamW",                    # Algorithme d’optimisation pour améliorer la convergence
+        mosaic=0.8,                           # Mélange d’images pour améliorer l'entrainement (Image inversée ou tournée)
+        scale=0.5,                            # Zoom aléatoire sur les images
+        fliplr=0.5,                           # Flip horizontal aléatoire (sur 50% des images)
     )
